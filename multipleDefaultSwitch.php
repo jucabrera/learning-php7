8ed80e4cef2e43aad93b7ff0050916d2b8afdcb9
Juciellen <jucarol17@gmail.com>
2017-12-02 00:00:13 -0200

    first commit

---
+++ multipleDefaultSwitch.php
@@ -0,0 +1,20 @@
+<?php
+$i = 10;
+
+switch ($i) {
+    case 0:
+        echo "i equals 0";
+        break;
+    case 1:
+        echo "i equals 1";
+        break;
+    case 2:
+        echo "i equals 2";
+        break;
+    default:
+        echo "default 1";
+        break;
+    default:
+        echo "default 1";
+}
+//Fatal error: Switch statements may only contain one default clause in /usr/local/zend/var/apps/http/__default__/0/learning-php7/1.0.0_5350/index.php on line 17
---
+++ phpTags.php
@@ -0,0 +1,2 @@
+<?="ee"?> //sempre habilitado mesmo com short_open_tag off
+<?php echo "short tags"?> //funciona se short_open_tag estiver on
